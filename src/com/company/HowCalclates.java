67cc059c66e77396387a5d2d077ba439e546f6cb
гк <DimaKorik>
2022-09-29 18:42:13 +0300

    commit

---
+++ src/com/company/HowCalclates.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class HowCalclates {
+
+    public static void main(String[] args) {
+        double g = 1./2/Math.sin(Math.PI/2);
+        System.out.println(g);
+
+    }
+}
